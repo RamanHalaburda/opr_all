e24a8ce784aad07b203a93f2de2b01bcfc0aa398
RomanHalaburda <mtbrider94@gmail.com>
2017-04-09 03:00:32 +0300

    Some

--- OPR1/Monte-Carlo.cs
+++ OPR1/Monte-Carlo.cs
@@ -11,7 +11,6 @@ namespace OPR1
         private double min;
         private List<ExtremumCoordinates> extremumCoordinatesList;
 
-        //public
         public Monte_Carlo()
         {
             extremumCoordinatesList = new List<ExtremumCoordinates>();
@@ -23,7 +22,6 @@ namespace OPR1
             return extremumCoordinatesList;
         }
 
-        //private
         public double monteCarlo()
         {
             double x1;
@@ -62,9 +60,6 @@ namespace OPR1
 
         private bool borderOn(double x1, double x2)
         {
-            /*  double first_border = Math.Pow(x1, 2) - 6 * x1 + 4 * x2 - 11;
-             double second_border = 3*x2 - x1*x2 + Math.Exp(x1-3) - 1;
-             if (first_border >= 0 && second_border >= 0)*/
             double first_border = 2 * Math.Pow(x1, 2) + 3 * Math.Pow(x2, 2);
             if (first_border <= 6 && x1 >= 0 && x2 >= 0)
             {
@@ -83,8 +78,6 @@ namespace OPR1
 
         private double f(double x1, double x2)
         {
-            //return Math.Pow(x1, 2) + Math.Pow(x2, 2) - 16 * x1 - 10 * x2;
-            //return 2 * x1 - Math.Pow(x1, 2) + Math.Pow(x2, 2);
             return (-6 * x1 + 2 * Math.Pow(x2, 2) - 2 * x1 * x2 + 2 * Math.Pow(x2, 2));
         }
     }
